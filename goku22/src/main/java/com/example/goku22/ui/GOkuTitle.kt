c1260964f698808a7c7b3706c3dfe2f6b95c62cc
Suraj Kumar <Suraj.Kumar@mobileprogramming.com>
2023-07-14 17:03:05 +0530

    Initial commit

---
+++ goku22/src/main/java/com/example/goku22/ui/GOkuTitle.kt
@@ -0,0 +1,9 @@
+package com.example.goku22.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun GokuTitle(title:String){
+    Text(text = title)
+}
